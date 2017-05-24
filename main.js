bd315398f4cc3a0938bfc4d58a6ce2cf905f2895
phgupta <phgupta@ucdavis.edu>
2017-05-24 02:01:18 -0700

    button on top of image

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function uploadButtonPressed() {
 
+	
 	var url = "http://138.68.25.50:7821/main";
 
 	var selectedFile = document.getElementById('fileSelector').files[0];
@@ -12,18 +13,26 @@ function uploadButtonPressed() {
 	oReq.onload = function() {
 		console.log(oReq.responseText);
 	}
-
 	oReq.send(formData);
 
-	/*
+
+	// Displays image to webpage
 	var image = document.getElementById('theImage');
+	var hamburgerButton = document.getElementById('hamburgerButton');
+	var imageContainer = document.getElementById('imageContainer');
 	var fr = new FileReader();
 
 	fr.onload = function () {
 		image.src = fr.result;
-		image.style.width = "300px";
+		image.style.width = "100%";
+		image.style.height = "100%";
+
+		imageContainer.style.position = "relative";
+		hamburgerButton.style.display = "inline-block";
+		hamburgerButton.style.position = "absolute";
+		hamburgerButton.style.bottom = "0%";
+		hamburgerButton.style.right = "0%";
 	};
 
 	fr.readAsDataURL(selectedFile);
-	*/
 }
