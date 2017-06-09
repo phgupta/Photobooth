e4e915f425b229ce3c2165c1b108c9c87eae7d6e
phgupta <phgupta@ucdavis.edu>
2017-06-09 12:25:56 -0700

    Project Complete

--- home.js
+++
@@ -1,4 +0,0 @@
-function enterPressed() {
-	console.log("Hi");
-	window.location = 'http://138.68.25.50:7821/main';
-}
--- makeDB.js
+++
@@ -1,7 +0,0 @@
-var sqlite3 = require("sqlite3").verbose();
-var dbFile = "photos.db";
-
-var db = new sqlite3.Database(dbFile);
-var cmdStr = "CREATE TABLE PhotoLabels (fileName TEXT UNIQUE NOT NULL PRIMARY KEY, labels TEXT, labelCount INTEGER, favorite INTEGER)"
-
-db.run(cmdStr);
